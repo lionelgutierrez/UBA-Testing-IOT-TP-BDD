cf360b778443f0fb0e5a614697ac2b8dcbce2dff
Lionel <lionelgutierrez@gmail.com>
2020-11-14 09:47:57 -0300

    caracteristica 1 y 2. Ajuste lista.js para normalizar idioma

--- src/lista.js
+++ src/lista.js
@@ -5,29 +5,29 @@ module.exports = class Lista {
         this.#elementos = [];
     }
 
-    add(key, value) {
-        this.#elementos.push({'clave': key, 'valor': value});
+    add(clave, valor) {
+        this.#elementos.push({'clave': clave, 'valor': valor});
     }
 
-    indexOf(key) {
+    indexOf(clave) {
         for (var indice = 0; indice < this.#elementos.length; indice++) {
-            if (this.#elementos[indice].clave == key) {
+            if (this.#elementos[indice].clave == clave) {
                 return indice;
             }
         }
         return NaN;
     }
 
-    delete(key) {
-        var indice = this.indexOf(key);
+    delete(clave) {
+        var indice = this.indexOf(clave);
         if (!isNaN(indice)) {
             this.#elementos.splice(indice, 1);
             return
         }
     }
 
-    find(key) {
-        var indice = this.indexOf(key);
+    find(clave) {
+        var indice = this.indexOf(clave);
         if (!isNaN(indice)) {
             return this.#elementos[indice].valor;
         }
@@ -38,61 +38,3 @@ module.exports = class Lista {
         return this.#elementos.length;
     }
 }
-/*
-    
-    constructor(){
-        
-    }
-        count() {
-        
-
-    }
-
-    find(clave) {
-        var i = this.indice(clave);
-        if (i >= 0) {
-            return this.#elementos[i].valor;
-        }
-        else {
-            return NaN;
-        }
-    }
-
-    add(clave,valor){
-        
-        var i = this.indice(clave);
-        if (i >= 0) {
-            this.#elementos[i].valor = valor;
-        }
-        else {
-            this.#elementos.push({clave,valor});  
-        }
-        
-    }
-
-    devolverListaArray() {
-        var listasalida = this.#elementos.slice();
-        listasalida.sort(function(a,b) {return ((a.clave < b.clave) ? -1 : ((a.clave >  b.clave) ? 1 : 0));});
-        return listasalida;
-    }
-
-    delete(clave) {
-        
-        var i = this.indice(clave);
-        if (i >= 0) {
-           this.#elementos.splice(i,1);
-        }
-    }
-
-    indice(clave) {
-        for(var i=0; i<this.#elementos.length;i++ )  {
-            if (this.#elementos[i].clave == clave) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-*/
-
-
